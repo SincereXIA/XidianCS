341594744a296288eeea8f252757989d5df553b3
Jack Bryant <akynazh@qq.com>
2023-06-10 11:22:16 +0800

    分布式系统：补充分布式论坛实验

---
+++ 专业选修课/分布式系统/作业/分布式论坛/源码参考/forum-service-8002/src/main/java/xdu/jzh/mapper/ReplyPostMapper.java
@@ -0,0 +1,11 @@
+package xdu.jzh.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import xdu.jzh.pojo.ReplyPost;
+
+/**
+ * @author akyna
+ * @date 05/30 030 5:07 PM
+ */
+public interface ReplyPostMapper extends BaseMapper<ReplyPost> {
+}
